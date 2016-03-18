1522cd42424c341eab2a0812c6a83e67c29b5e7c
hejiangda <hejiangda@gmail.com>
2016-03-18 18:01:56 +0800

    new

--- CHTP/test2.c
+++ CHTP/test2.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
 
-void main(){
-  int i;
+void main()
+{int a,b,l,s;
+  printf("%d,%d\n",l=2*(a+b),s=a*b );
 
-  for(i=0;i<80;i++){
-    printf("*");
-  }
-  printf("\n");
-  printf("\t\t\tThis is my first C program!\n");
-  for(i=0;i<80;i++){
-    printf("*");
-  }
-  printf("\n");
+}
 
+#include <stdio.h>
+
+void main()
+{char a;
+  printf("%d\n",a );
+}
+
+
+#include <stdio.h>
+
+void main()
+{int a,b,c,x;
+  printf("%d\n",x=(a++b+c)/3 );
+}
+
+#include <stdio.h>
+
+void main()
+{int a,b,c;
+  double y;
+  y=(3.8*(b*b+a*c))/(b*a);
+  printf("%f\n",y);
 }
